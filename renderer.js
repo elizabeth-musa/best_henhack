b42d2837401b692e1351f7b21c00a1816ddebe15
Elizabeth Musa <emusa@udel.edu>
2026-03-01 00:52:34 -0500

    modified:   index.html 	modified:   renderer.js 	modified:   settings.html

--- renderer.js
+++ renderer.js
@@ -1,2 +1,14 @@
 // Renderer process code
 console.log('Renderer process started');
+const dateElement = document.getElementById("date");
+
+const today = new Date();
+
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+};
+
+dateElement.textContent = today.toLocaleDateString(undefined, options);
